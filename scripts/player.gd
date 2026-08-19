8e179a6c300bb7a77c94891d672cac2d7ef6ff61
hex4 <git@hex4.xyz>
2026-08-18 18:02:17 -0600

    make player movement and start on the grapple!

---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+enum State {FREE, ATTACHED}
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+
+var state := State.FREE
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("grapple"):
+		state = State.ATTACHED
+	else:
+		state = State.FREE
+	
+	if state == State.FREE:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		move_and_slide()
