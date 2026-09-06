36dc701ba1a2058b53785c4e4e5018168a81c951
hex4 <git@hex4.xyz>
2026-09-05 21:19:53 -0600

    lots of progress on grapple improvements!

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,9 +7,13 @@ const JUMP_VELOCITY = -300.0
 const MAX_GRAPPLE_RANGE = 180
 const ALIGNMENT_THRESHOLD = 0.7
 const DISTANCE_WEIGHT = 0.3
+const GRAPPLE_NUDGE_SPEED = 150
+const GRAVITY = 1000.0
+const AIR_STEER_MULT = 1
 
 var state := State.FREE
 var attached_point: Node2D
+var rope_length: float
 
 func get_grapple_point() -> Node2D:
 	var points: Array[Node] = get_tree().get_nodes_in_group("attachable")
@@ -39,40 +43,73 @@ func _physics_process(delta: float) -> void:
 		attached_point = get_grapple_point()
 		if attached_point:
 			(attached_point.get_node("Icon") as Sprite2D).modulate = Color("red")
+			rope_length = position.distance_to(attached_point.position)
 	elif Input.is_action_just_released("grapple"):
 		if attached_point:
 			(attached_point.get_node("Icon") as Sprite2D).modulate = Color(1,1,1,1)
 		attached_point = null
+		rope_length = 0
 	
 	if Input.is_action_pressed("grapple"):
 		state = State.ATTACHED
+		if (len($Line2D.points) > 1):
+			$Line2D.remove_point(1)
+		if attached_point:
+			$Line2D.add_point(attached_point.position - position)
+		
 	else:
+		if len($Line2D.points) > 1:
+			$Line2D.remove_point(1)
 		state = State.FREE
 		attached_point = null
 		
 	
 	
 	if state == State.FREE:
-		# Add the gravity.
+		# In the air:
 		if not is_on_floor():
-			velocity += get_gravity() * delta
-
-		# Handle jump.
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * SPEED
+			velocity += Vector2.DOWN * GRAVITY * delta
+			
+			var direction := Input.get_axis("left", "right")
+			
+			if direction:
+				if velocity.x * direction <= SPEED:
+					velocity.x = move_toward(velocity.x, SPEED * direction, SPEED * AIR_STEER_MULT * delta)
+			
+			move_and_slide()
+		# On the ground.
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			# Handle jump.
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
 
-		move_and_slide()
+			# Get the input direction and handle the movement/deceleration.
+			# As good practice, you should replace UI actions with custom gameplay actions.
+			var direction := Input.get_axis("left", "right")
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+
+			move_and_slide()
 		
 	if state == State.ATTACHED:
 		if not is_on_floor():
-			velocity += get_gravity() * delta
+			velocity += Vector2.DOWN * GRAVITY * delta
+
+		var input_direction := Input.get_axis("left", "right")
+		var forwards_on_circle = (attached_point.position - position).rotated(deg_to_rad(90)).normalized()
+		print(forwards_on_circle)
+		velocity += input_direction * forwards_on_circle * GRAPPLE_NUDGE_SPEED * delta
+
+
+		var predicted = position + velocity * delta
 		
+		# constrain prediction onto a circle with radius rope_length
+		
+		var target = attached_point.position + (predicted - attached_point.position).normalized() * rope_length
+		
+		velocity = (target - position) / delta
+		
+		move_and_slide()
 		
